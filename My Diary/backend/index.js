6386b0e2628344e2521bd2f74e4a5a4829558157
AkashAmirisetty <amirisettyakash@gmail.com>
2025-09-12 18:13:03 +0530

    backend

---
+++ My Diary/backend/index.js
@@ -0,0 +1,38 @@
+const express=require('express');
+const cors=require('cors');
+const bcrypt=require('bcrypt');
+
+const app=express();
+const port=3000
+
+// middilewares
+app.use(cors());
+// To access the form data in the login page
+app.use(express.urlencoded({extended:true}));
+
+
+
+
+
+
+
+
+
+app.get('/', (req,res)=>{
+    console.log(req)
+    res.status(200).json({message:'Successful'})
+})
+
+app.post('/registerUser',(req,res)={
+    res.status(200).json({message: 'User Registered'})
+})
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
+
+
+
